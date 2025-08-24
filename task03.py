2e1f6bd154e319a40543f892282215a91b929c69
JasurbekAbdullayev1520 <jasurbek.abdullayev1520@gmail.com>
2025-08-24 22:26:44 +0500

    task04 ni bajardim

---
+++ task03.py
@@ -0,0 +1,7 @@
+harf=str(input("Harf kiritng:"))
+
+if harf== in(A,Z):
+    print("Harf katta")
+
+if harf=='a''z':
+    print("Harf kichkina")
---
+++ task04.py
@@ -0,0 +1,10 @@
+son=float(input("Son kiriting:"))
+
+if son>0:
+    print('Ijobiy son')
+
+if son<0:
+    print('Manfiy son')
+
+if son==0:
+    print('Nol')
