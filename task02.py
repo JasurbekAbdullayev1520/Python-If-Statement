dba28552540ef9d9126ac0cb2d60cdd8483a88f0
JasurbekAbdullayev1520 <jasurbek.abdullayev1520@gmail.com>
2025-08-24 22:00:25 +0500

    task02 ni bajardim

---
+++ task02.py
@@ -0,0 +1,12 @@
+son1=int(input("1-sonni kiriting:"))
+
+son2=int(input("2-sonni kirinting:"))
+
+if son1>son2:
+    print("1-son katta")
+
+if son1<son2:
+    print("2-son katta")
+
+if son1==son2:
+    print("Teng")
